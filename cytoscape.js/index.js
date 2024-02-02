a9d3bef38f14fda4ef3f782cf9e04b3ff9e31b18
fbartusch <felix.bartusch@uni-tuebingen.de>
2024-02-02 17:16:37 +0100

    Add node styles for components
    The node styles mimick the styles used in the PROV-DM
    documentation at https://www.w3.org/TR/prov-dm/#section-example-one

--- cytoscape.js/index.js
+++ cytoscape.js/index.js
@@ -3,41 +3,68 @@ var cy = cytoscape({
     container: document.getElementById('cy'), // container to render in
   
     elements: [ // list of graph elements to start with
-      { // node a
-        data: { id: 'a' }
-      },
-      { // node b
-        data: { id: 'b' }
-      },
       {
-        // node agent
+        // Entity node
+        data: { id: 'entity', type: 'entity'}
+      },
+
+      {
+        // Activity node
+        data: { id: 'activity', type: 'activity'}
+      },
+
+      {
+        // Agent node
         data: { id: 'agent', type: 'agent'}
       },
-      { // edge ab
-        data: { id: 'ab', source: 'a', target: 'b' }
-      }
     ],
+
+    
   
     style: [ // the stylesheet for the graph
       {
         selector: 'node',
         style: {
-          'background-color': '#666',
           'label': 'data(id)'
         }
       },
-
+      // Entity node Style
+      {
+        selector: 'node[type="entity"]',
+        style: {
+          'shape': 'ellipse',
+          'width': '75px',
+          'height': '40px',
+          'background-color': '#fffc87',
+          'border-width': '1px',
+          'border-color': '#353aff'
+        }
+      },
+      // Activity node Style
+      {
+        selector: 'node[type="activity"]',
+        style: {
+          'shape': 'rectangle',
+          'width': '40px',
+          'height': '40px',
+          'background-color': '#9fb1fc',
+          'border-width': '1px',
+          'border-color': '#353aff'
+        }
+      },
+      // Agent node style
       {
         selector: 'node[type="agent"]',
         style: {
-          // TODO Check shape-polygon-points
           'shape': 'polygon',
+          'shape-polygon-points': '1, 1, -1 1, -1, 0, 0, -1, 1, 0',
+          'width': '40px',
+          'height': '40px',
           'background-color': '#e27602',
           'border-width': '1px',
           'border-color': 'black'
         }
       },
-  
       {
         selector: 'edge',
         style: {
@@ -57,15 +84,17 @@ var cy = cytoscape({
   
   });
 
-  cy.add({
-    group: 'nodes',
-    data: { weight: 75 },
-    position: { x: 200, y: 200 }
-    });
 
-// can use reference to eles later
-var eles = cy.add([
-    { group: 'nodes', data: { id: 'n0' }, position: { x: 100, y: 100 } },
-    { group: 'nodes', data: { id: 'n1' }, position: { x: 200, y: 200 } },
-    { group: 'edges', data: { id: 'e0', source: 'n0', target: 'n1' } }
-  ]);
+
+//  cy.add({
+//    group: 'nodes',
+//    data: { weight: 75 },
+//    position: { x: 200, y: 200 }
+//    });
+//
+//// can use reference to eles later
+//var eles = cy.add([
+//    { group: 'nodes', data: { id: 'n0' }, position: { x: 100, y: 100 } },
+//    { group: 'nodes', data: { id: 'n1' }, position: { x: 200, y: 200 } },
+//    { group: 'edges', data: { id: 'e0', source: 'n0', target: 'n1' } }
+//  ]);
